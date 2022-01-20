e56522d91d3be1959b94df06af15141ba77d087a
nphuyy1901 <66677307+nphuyy1901@users.noreply.github.com>
2022-01-20 15:26:24 +0700

    DataBase

---
+++ data_Insert.sql
@@ -0,0 +1,321 @@
+use DB_Recipe;
+-- chu de
+insert into chude(ten,mota,hinhanh) 
+value (N'Ăn sáng: Tổng hợp món ăn sáng dễ làm tại nhà.',
+		N'Sau một đêm ngủ ngon giấc, cơ thể thức dậy với cái dạ dày trống rỗng vì đã tiêu ngốn hết năng lượng nạp vào từ bữa tối hôm qua. Vì lẽ đó mà bữa ăn sáng là bữa ăn vô cùng quan trọng cung cấp năng lượng cần thiết cho cơ thể để khởi đầu một ngày mới',
+        'Image/chude/ansang.png');
+        
+insert into chude(ten,mota,hinhanh) 
+value (N'Ăn tối: Ngon điên đảo CÁC MÓN ĂN TỐI ai ăn cùng nghiền.',
+		N'Những điều mới lạ trên mâm cơm luôn là niềm vui nho nhỏ của các chị em phụ nữ. Không cần quá băn khoăn lo lắng về điều đó nhé, hãy cùng WikiRecipe xem qua những món ăn gây nghiện này thôi!',
+        'Image/chude/antoi.png');
+insert into chude(ten,mota,hinhanh) 
+value (N'Ăn trưa: Trưa nay ăn gì?',
+		N'“Trưa nay ăn gì?” có lẽ là câu hỏi đau đầu nhất đối với dân văn phòng khi loanh quanh mãi vẫn chỉ là những hàng cơm, bún, phở bình dân quen thuộc đến phát ngán. Vậy thì hôm nay hãy thử “đổi gió” với bộ sưu tập các món ăn trưa ngon sau đây nhé!',
+        'Image/chude/antrua.png');
+insert into chude(ten,mota,hinhanh) 
+value (N'Dã ngoại: Đi picnic ăn gì? Mách nhỏ những đồ ăn đi picnic tiện lợi nhất.',
+		N'Lên thực đơn để mua sắm đồ ăn đi picnic luôn làm chúng ta đau đầu. Vì tối nay còn chưa biết ăn gì, nay lại lên thực đơn cho cả nhóm thì ai cũng lo lắng. Hồ Đồng Đò chia sẻ cho bạn những món ăn đi pinic, đi cắm trại thiết thực nhất. Đầy đủ, ngon miệng lại tiết kiệm chi phí. Mong sau những chia sẻ này bạn sẽ khỏi nhức đầu vì câu hỏi “Đi picnic ăn gì?”',
+        'Image/chude/dangoai.png');
+insert into chude(ten,mota,hinhanh) 
+value (N'Các món ăn Giáng Sinh thơm ngon đơn giản hấp dẫn cho gia đình',
+		N'Ngày lễ Giáng Sinh sắp đến gần là dịp để mọi người quây quần bên nhau trong không khí ấm áp và hạnh phúc. Hãy cùng vào bếp với mình tham khảo một số món ăn Giáng Sinh dành cho ngày lễ đặc biệt này để giúp các bạn có một mùa Giáng Sinh thật ý nghĩa nhé!',
+        'Image/chude/giangsinh.png');
+        
+insert into chude(ten,mota,hinhanh) 
+value (N'Tổng hợp các món ăn kinh dị trong ngày Halloween bạn không thể bỏ qua',
+		N'Nếu bạn đang có dự định tổ chức một bữa tiệc Halloween tại nhà và đang bối rối không biết sẽ làm món ăn nào ngon phù hợp cho bữa tiệc thì hãy cùng chuyên mục Mẹo vào bếp của mình vào bếp và tổng hợp lại các món ăn kinh dị siêu ngon, đơn giản tại nhà nhé!',
+        'Image/chude/halloween.png');
+insert into chude(ten,mota,hinhanh) 
+value (N'Lễ Tình nhân: Top những món ăn dễ nấu tặng người yêu.',
+		N'Mùa Valentine mới sắp tới rồi, bạn đã có dự định gì cho một nửa của mình chưa? Nếu bạn đang băn khoăn, lo lắng không biết tặng gì thì đây sẽ là những gợi ý tuyệt vời cho bạn. Chẳng còn gì tuyệt vời hơn là tự tay nấu một món ăn thật ý nghĩa cho các chàng, các nàng của mình trong ngày Valentine.',
+        'Image/chude/letinhnhan.png');
+insert into chude(ten,mota,hinhanh) 
+value (N'Tổng hợp những món chay ngon dễ làm hàng ngày',
+		N'Bạn là một tín đồ ăn chay và đang tìm kiếm những món chay ngon dễ làm? Ẩm thực chay cũng rất phong phú với nhiều món ăn được chế biến đa dạng. Cùng tham khảo bài viết này để cập nhật những món ăn chay ngon dễ thực hiện và tự tin trổ tài nấu nướng nhé.',
+        'Image/chude/monchay.png');
+
+insert into chude(ten,mota,hinhanh) 
+value (N'Các món ăn không thể thiếu trong mâm cỗ ngày Tết, giúp Tết thêm tròn vị',
+		N'Ẩm thực là một nét văn hóa của người Việt Nam, vì thế, mâm cỗ ngày Tết rất được mọi người chú trọng. Mâm cỗ có thể lớn hay nhỏ tùy gia đình và từng vùng miền. Tuy nhiên, có những món ăn hầu như không thể thiếu. Dưới đây là những món ăn mang đậm nét cổ truyền, bản sắc trong ngày Tết ở nước ta mà bạn có thể dễ dàng làm được.',
+        'Image/chude/ngaytet.png');
+insert into chude(ten,mota,hinhanh) 
+value (N'Thực đơn ngon đãi tiệc sinh nhật đơn giản tại nhà',
+		N'Sắp đến ngày sinh nhật của bạn hay người thân trong gia đình và bạn vẫn còn đang loay hoay tìm kiếm một thực đơn sinh nhật đãi tiệc đơn giản tại nhà. Hãy dựa vào đối tượng khách mời hoặc bạn có thể làm cuộc khảo sát nhỏ về nhu cầu của mỗi người, sau đó tổng hợp và chọn lọc ra những món ngon mỗi ngày sao cho phù hợp nhất',
+        'Image/chude/sinhnhat.png');
+-- the loai thuoc chu de ansang 
+
+insert into theloai(tentheloai) value (N'Phở');
+insert into theloai(tentheloai) value (N'Bánh mì xíu mại');
+insert into theloai(tentheloai) value (N'Bánh mì pate');
+insert into theloai(tentheloai) value (N'Xôi');
+
+insert into chude_theloai value(1,1);
+insert into chude_theloai value(1,2);
+insert into chude_theloai value(1,3);
+insert into chude_theloai value(1,4);
+
+-- the loai thuoc chu de antoi
+insert into theloai(tentheloai) value (N'Cơm chiên');
+insert into theloai(tentheloai) value (N'Lẩu gà');
+insert into theloai(tentheloai) value (N'Thịt kho');
+insert into theloai(tentheloai) value (N'Cá kho');
+
+insert into chude_theloai value(2,5);
+insert into chude_theloai value(2,6);
+insert into chude_theloai value(2,7);
+insert into chude_theloai value(2,8);
+
+-- the loai thuoc chu de antrua
+insert into theloai(tentheloai) value (N'Rau luộc');
+insert into theloai(tentheloai) value (N'Rau xào');
+insert into theloai(tentheloai) value (N'Thịt luộc');
+
+insert into chude_theloai value(3,5);
+insert into chude_theloai value(3,9);
+insert into chude_theloai value(3,10);
+insert into chude_theloai value(3,11);
+
+-- the loai thuoc chu de dangoai
+insert into theloai(tentheloai) value (N'Kimbap');
+insert into theloai(tentheloai) value (N'Bánh mì');
+insert into theloai(tentheloai) value (N'Hamburger');
+insert into theloai(tentheloai) value (N'Spaghetti');
+
+insert into chude_theloai value(4,12);
+insert into chude_theloai value(4,13);
+insert into chude_theloai value(4,14);
+insert into chude_theloai value(4,15);
+
+-- the loai thuoc chu de giangsinh
+insert into theloai(tentheloai) value (N'Gà tây');
+insert into theloai(tentheloai) value (N'Bánh pudding');
+insert into theloai(tentheloai) value (N'Bánh pate');
+
+insert into chude_theloai value(5,16);
+insert into chude_theloai value(5,17);
+insert into chude_theloai value(5,18);
+
+-- the loai thuoc chu de halloween
+insert into theloai(tentheloai) value (N'Súp bí đỏ');
+insert into theloai(tentheloai) value (N'Kẹo táo');
+insert into theloai(tentheloai) value (N'Bánh linh hồn');
+insert into theloai(tentheloai) value (N'Colcannon');
+
+insert into chude_theloai value(6,19);
+insert into chude_theloai value(6,20);
+insert into chude_theloai value(6,21);
+insert into chude_theloai value(6,22);
+
+-- the loai thuoc chu de letinhnhan
+insert into theloai(tentheloai) value (N'Cupcake');
+insert into theloai(tentheloai) value (N'Socolate');
+insert into theloai(tentheloai) value (N'Bánh kem');
+insert into theloai(tentheloai) value (N'Cookies');
+
+insert into chude_theloai value(7,23);
+insert into chude_theloai value(7,24);
+insert into chude_theloai value(7,25);
+insert into chude_theloai value(7,26);
+
+-- the loai thuoc chu de monchay
+insert into theloai(tentheloai) value (N'Đậu hũ');
+insert into theloai(tentheloai) value (N'Nui');
+insert into theloai(tentheloai) value (N'Lẩu nấm');
+insert into theloai(tentheloai) value (N'Cơm gạo lứt');
+
+insert into chude_theloai value(8,27);
+insert into chude_theloai value(8,28);
+insert into chude_theloai value(8,29);
+insert into chude_theloai value(8,30);
+
+-- the loai thuoc chu de ngaytet
+insert into theloai(tentheloai) value (N'Bánh chưng');
+insert into theloai(tentheloai) value (N'Thịt kho tàu');
+insert into theloai(tentheloai) value (N'Khổ qua');
+insert into theloai(tentheloai) value (N'Dưa hành');
+
+insert into chude_theloai value(9,31);
+insert into chude_theloai value(9,32);
+insert into chude_theloai value(9,33);
+insert into chude_theloai value(9,34);
+
+-- the loai thuoc chu de sinhnhat
+insert into theloai(tentheloai) value (N'Gỏi');
+insert into theloai(tentheloai) value (N'Bánh kem');
+insert into theloai(tentheloai) value (N'Tôm');
+insert into theloai(tentheloai) value (N'Gà luộc');
+
+insert into chude_theloai value(10,35);
+insert into chude_theloai value(10,36);
+insert into chude_theloai value(10,37);
+insert into chude_theloai value(10,38);
+
+insert into user(email,password,status,fullname,DOB,avatar) 
+value('FuocHuy@gmail.com','huydeptrai',1,N'Nguyễn Phước Huy','2001-7-10','Image/aboutus/Huy.png');
+
+insert into user(email,password,status,fullname,DOB,avatar) 
+value('HongDuc@gmail.com','123',1,N'Đàm Hồng Đức','2001-8-30','Image/aboutus/Đức.png');
+
+insert into user(email,password,status,fullname,DOB,avatar) 
+value('BinhMinh@gmail.com','huydeptrai',1,N'Lê Thái Bình Minh','2001-11-30','Image/aboutus/Minh.png');
+
+select * from theloai;
+select * from chude;
+
+insert into event(iduser,tenuser,ten,mota,tgbatdau,tgketthuc,soluong,hinhanh,hinhquangcao,giaithuong) 
+value(1,N'Nguyễn Phước Huy',N'Cuộc thi làm món nhậu đơn giản - bình dân - đãi khách',N'Bạn đang cần tham khảo một số món nhậu đơn giản đãi khách tại nhà. Phân vân không biết nên thực đơn như thế nào cho hợp lý. Lựa chọn món nhậu ngon mà cả bạn và khách đều thích. Cùng lai dai, tỉ tê xem bóng đá, chém gió vui vẻ. Vậy tham gia ngay sự kiện này để chia sẻ những món nhậu ngon của bạn cho mọi người cùng biết nhé!','2022-01-17','2022-05-17',0,'Image/EventDefault/bepnhau/bepnhau.png','Image/EventDefault/bepnhau/hinhqc_bepnhau.jpg','10 thùng bia tiger crystal');
+
+
+insert into event(iduser,tenuser,ten,mota,tgbatdau,tgketthuc,soluong,hinhanh,hinhquangcao,giaithuong) 
+value(1,N'Nguyễn Phước Huy',N'Cuộc thi chế biến bữa sáng đơn giản và đầy đủ dinh dưỡng',N'Sau một đêm ngủ ngon giấc, cơ thể thức dậy với cái dạ dày trống rỗng vì đã tiêu ngốn hết năng lượng nạp vào từ bữa tối hôm qua. Vì lẽ đó mà bữa ăn sáng là bữa ăn vô cùng quan trọng. Việc tự chuẩn bị bữa sáng thịnh soạn cho bản thân và các thành viên trong gia đình là một nét văn hóa thật đẹp của truyền thống Á Đông. Cuộc thi nhằm tìm ra những ý tưởng chế biến món ăn tuyệt vời. Hãy tham gia ngay để nhận những phần quà hấp dẫn nhé!!!','2022-01-17','2022-05-17',0,'Image/EventDefault/buasangdongian/hinh.png','Image/EventDefault/buasangdongian/hinhqc.png','1 vé tham dự Master Chef của Mỹ');
+
+insert into event(iduser,tenuser,ten,mota,tgbatdau,tgketthuc,soluong,hinhanh,hinhquangcao,giaithuong) 
+value(2,N'Đàm Hồng Đức',N'Cuộc thi chế biến các món thơm ngon bổ dưỡng từ gà',N'Gà là loại thực phẩm quen thuộc, có thể biến tấu thành nhiều món ăn đa dạng. Hãy dùng sự sáng tạo của bạn để tạo ra những món ăn ngon với nguyên liệu là gà nhé. Tham gia ngay sự kiện chỉ với 1 click','2022-01-17','2022-05-17',0,'Image/EventDefault/lamga/hinh.png','Image/EventDefault/lamga/hinhqc.jpg','1.000.000 VNĐ');
+
+insert into event(iduser,tenuser,ten,mota,tgbatdau,tgketthuc,soluong,hinhanh,hinhquangcao,giaithuong) 
+value(3,N'Lê Thái Bình Minh',N'Cuộc thi tìm ra "Vua bánh mì"',N'Cuộc thi nhắm tìm ra những ông vua chế biến bánh mì, những người có đầu óc nghệ thuật và tính thẩm mĩ cao để làm ra những chiếc bánh mì ngon và đặc biệt nhất. Tham gia ngay với chúng tôi để nhận những phần quà hấp dẫn nhe','2022-01-17','2022-05-17',0,'Image/EventDefault/vuabanhmi/hinh.png','Image/EventDefault/vuabanhmi/hinhqc.png','1 năm ăn bánh mì ở vuabanhmichamcom.com miễn phí');
+
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (1,1,1,
+N'Phở Tái Đơn Giản Với Viên Súp Phở Bò',
+N'Viên súp phở bò thật sự tiện lợi những khi mình không có nhiều thời gian mà các mem trong nhà lại muốn ăn phở.
+Món phở tái dã chiến này nấu nhanh, thơm ngon, tuy không bằng hầm 2 tiếng bằng xương bò thật nhưng được cái tiện lợi cho những ngày bận rộn.',
+N'400 gr bò phi lê
+4 Trứng gà
+1 củ hành tây
+200 gr giá
+1 củ cải trắng
+Vài tai hồi, quế, đinh hương
+2 viên súp phở bò
+Nước mắm, đường, tiêu
+Hành lá, rau răm, rau húng quế
+Tương đen, tương ớt ăn phở
+Bánh phở khô' ,
+'Image/Recipe/photai.png',
+0, '2022-01-18', 60);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (1,2,5,
+N'Cơm chiên kimchi bò xúc xích',
+N'Nay dọn tủ, để mai đón năm mới mọi người ơi. Tận dụng các nguyên liệu mỗi thứ còn một tí cơm nguội, ít bò, xúc xích kim chi nhà làm… em có món ăn nóng hổi rất thích hợp với khí trời lành lạnh mấy nay a :))',
+N'2 chén cơm nguội
+1 chén kim chi
+2 cây xúc xích
+Lưng chén bò lát sẵn (200g)
+1/3 quả ớt chuông
+Hành tây, tỏi, hành tím, hành ngò
+Gia vị sốt trộn: 1/2mc tương đậu hàn (có 3 loại nhà có sẵn), 1mc dầu hào, 1mc nước tương, 1mc dầu mè
+1-2 quả Trứng ốp lết' ,
+'Image/Recipe/comchien.png',
+0, '2022-01-18', 30);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (1,3,9,
+N'Rau luộc chấm mắm kho quẹt',
+N'Rau luộc đê cả nhà ơi',
+N'Rau yêu thích
+Nước mắm
+Dầu ăn
+Tỏi băm
+Ớt, tiêu
+Đường
+Bột ngọt
+Hành lá' ,
+'Image/Recipe/rauluoc.png',
+0, '2022-01-18', 15);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (2,4,12,
+N'Kimbap chay',
+N'Nhà có mẹ ăn chay nên thi thoảng em sẽ ra cả món chay để đổi vị cho cả nhà luôn ạ. Mọi người thử nha ăn bon miệng lắm luôn đó!',
+N'Lá rong biển cuộn cơm
+Dưa chuột
+Cà rốt
+Đậu phụ
+Nấm đùi gà
+Cơm' ,
+'Image/Recipe/kimbap.png',
+0, '2022-01-18', 50);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (2,5,16,
+N'Gà Tây đút lò',
+N'Họp mặt tất niên có món Ta thì cũng phải có món Tây, ngoài món ăn Việt tôi còn làm thêm món gà Tây đút lò ăn kèm cùng bánh mì pumpkin. Món này gần như được ăn trong mùa lễ Tạ ơn hay Noel cuối năm',
+N'1/2 con gà tây (turkey)
+1 chén hành tím, tỏi đập dập
+2 tbsp dầu táo olive(fresh apple)
+1 tbsp dấm đỏ(vinegar)
+2 tbsp tương gourmet hay dầu hào
+1 tbsp ớt màu
+1 tbsp lá mint nhuyễn hay bột
+1/2 miếng chanh' ,
+'Image/Recipe/gataydutlo.png',
+0, '2022-01-18', 90);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (2,6,19,
+N'Kẹo dẻo trái cây cho bé 6+ (kẹo chíp chíp)',
+N'Nhà có dưa hấu nhà nên tranh thủ làm cho ken ăn',
+N'80 ml nước ép dưa hấu,có thể thay nước ép khác táo,xoài,cam lê
+9 g Gelatin (hoặc 2,5 lá gelatin)' ,
+'Image/Recipe/keotao.png',
+0, '2022-01-18', 180);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (3,7,25,
+N'Bánh kem xoài',
+N'Món bánh mình làm theo công thức của em @dibee. Rất ngon và rất đáng để làm ạ!',
+N'1 quả xoài ngọt (nếu xoài chua là vô duyên lắm á!)
+100 g bánh quy giòn
+300 ml kem tươi' ,
+'Image/Recipe/banhkemxoai.png',
+0, '2022-01-18', 120);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (3,8,27,
+N'Chả đậu hũ rau răm',
+N'Đậu hũ cùng rau răm cũng khá thơm và lạ miệng ạ. Chả bùi bùi, sườn dai dai. Vỏ ngoài dòn, bên trong mềm béo',
+N'Đậu hũ
+Sườn chay
+Rau răm
+Hành' ,
+'Image/Recipe/dauhu.png',
+0, '2022-01-18', 60);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (3,9,32,
+N'Thịt kho tàu (bằng coca)',
+N'Mùa lạnh ăn món này với cơm thì bon mồm vô cùng, tốn cơm lắm đấy',
+N'300 g thịt ba chỉ
+1 lon coca
+10 quả trứng cút
+Bột nêm, nước mắm, đường, dầu ăn, dầu hảo
+Hành khô, tỏi khô
+Hành lá' ,
+'Image/Recipe/thitkhotau.png',
+0, '2022-01-18', 120);
+
+insert into recipe(iduser, idchude, idtheloai ,ten, mota , nguyenlieu , hinhanh, luotthich , tgdang , tgthuchien) 
+value (3,10,35,
+N'Gỏi cuốn rau mầm cải đỏ',
+N'Hây..zaaa! Có công vun xới, có ngày hưởng rau là đây mà!. Sáng nay mình mới thu hoạch rau mầm cải đỏ thế là trưa nay đã có ngay một món cuốn ngon lành rồi đó! Mầm cải tươi ngọt, lạp xưởng nhà làm chắc nụi chiên cho sém vàng thơm ơi là thơm, cuộn cùng các loại rau củ giòn giòn, chấm cùng sốt bơ đậu phộng béo béo nóng hổi nữa thì... Ôi sao mà ngon dữ! Yummy...',
+N'2 củ sắn --> cắt sợi, xào chín
+1 quả ớt chuông đỏ --> rửa sạch, bổ đôi, bỏ hột, cắt sợi
+2 củ cà rốt --> bào sợi, xào chín
+5 cây xà lách mỡ --> rửa sạch, để ráo
+200 g rau mầm cải đỏ --> rửa sạch, để ráo
+3 cây lạp xưởng --> chiên vàng xắt mỏng
+1 xấp bánh tráng
+SỐT chấm:
+8 M bơ đậu phộng
+8 trái tắc (hoặc 8m nước cốt chanh)
+8 m nước tương
+1 nhánh tỏi băm nhuyễn (hoặc 1M đầu hành boaro băm nhuyễn)
+1 m mật mía
+1 trái ớt
+100 ml nước
+1/2 chén nhỏ đậu phộng rang thơm, giã nhỏ' ,
+'Image/Recipe/goi.png',
+0, '2022-01-18', 40);
---
+++ dbrecipeInit.sql
@@ -0,0 +1,161 @@
+create database if not exists DB_Recipe;
+
+use DB_Recipe;
+
+
+CREATE TABLE IF NOT EXISTS user(
+	id INT auto_increment,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(30) NOT NULL,
+    status INT,
+	fullname NVARCHAR(50),
+    DOB DATE,
+    avatar VARCHAR(255),
+    primary key(id)
+);
+
+CREATE TABLE IF NOT EXISTS recipe(
+	id INT auto_increment,
+    iduser INT NOT NULL,
+    idchude INT NOT NULL,
+    idtheloai INT NOT NULL,
+    ten NVARCHAR(255) NOT NULL UNIQUE,
+    mota NVARCHAR(1000) NOT NULL,
+    nguyenlieu NVARCHAR(1000) NOT NULL,
+    hinhanh VARCHAR(255) NOT NULL,
+	luotthich INT,
+    tgdang DATE NOT NULL,
+    tgthuchien INT NOT NULL,
+    
+    PRIMARY KEY(id)
+);
+CREATE TABLE IF NOT EXISTS step(
+	id INT auto_increment,
+    buoc INT,
+    idrecipe INT,
+    hinhanh VARCHAR(255),
+    des NVARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+CREATE TABLE IF NOT EXISTS chude(
+	id INT auto_increment,
+    ten NVARCHAR(255) NOT NULL,
+    mota NVARCHAR(500) NOT NULL,
+    hinhanh VARCHAR(255) NOT NULL,
+   
+   PRIMARY KEY(id)
+);
+CREATE TABLE IF NOT EXISTS chude_theloai(
+	idchude INT,
+    idtheloai INT,
+    PRIMARY KEY(idchude,idtheloai)
+);
+
+CREATE TABLE IF NOT EXISTS theloai(
+	id INT auto_increment,
+    tentheloai NVARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS event(
+	id INT auto_increment,
+    iduser INT NOT NULL,
+    tenuser NVARCHAR(255),
+    ten NVARCHAR(255) NOT NULL,
+    mota NVARCHAR(500) NOT NULL,
+    tgbatdau DATE NOT NULL,
+    tgketthuc DATE NOT NULL,
+    soluong INT NOT NULL,
+    hinhanh VARCHAR(255) NOT NULL,
+    hinhquangcao VARCHAR(255) NOT NULL,
+    giaithuong VARCHAR(255) NOT NULL,
+    
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS album(
+	id INT auto_increment,
+	id_user INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS comment(
+    id INT auto_increment,
+    idrecipe INT, 
+
+    PRIMARY KEY(id)
+);
+CREATE TABLE IF NOT EXISTS chitiet_comment(
+	idcomment INT,
+    iduser INT,
+    content NVARCHAR(255),
+    ngaydang DATE,
+    PRIMARY KEY(idcomment,iduser)
+);
+
+
+CREATE TABLE IF NOT EXISTS album_recipe(
+	album_id INT,
+    recipe_id INT,
+    
+    PRIMARY KEY(album_id,recipe_id)
+);
+CREATE TABLE IF NOT EXISTS thamgiaevent(
+	iduser INT,
+    idevent INT,
+    idrecipe INT,
+    primary key(idrecipe,idevent)
+);
+
+ALTER TABLE album ADD FOREIGN KEY(id_user) REFERENCES user(id);
+ALTER TABLE album_recipe ADD FOREIGN KEY(album_id) REFERENCES album(id);
+ALTER TABLE album_recipe ADD FOREIGN KEY(recipe_id) REFERENCES recipe(id);
+
+ALTER TABLE comment ADD FOREIGN KEY(idrecipe) REFERENCES recipe(id);
+
+ALTER TABLE chitiet_comment ADD FOREIGN KEY(idcomment) REFERENCES comment(id);
+ALTER TABLE chitiet_comment ADD FOREIGN KEY(iduser) REFERENCES user(id);
+
+ALTER TABLE event ADD FOREIGN KEY(iduser) REFERENCES user(id);
+
+ALTER TABLE thamgiaevent ADD FOREIGN KEY(iduser) REFERENCES user(id);
+ALTER TABLE thamgiaevent ADD FOREIGN KEY(idevent) REFERENCES event(id);
+ALTER TABLE thamgiaevent ADD FOREIGN KEY(idrecipe) REFERENCES recipe(id);
+
+ALTER TABLE recipe ADD FOREIGN KEY(iduser) REFERENCES user(id);
+
+ALTER TABLE step ADD FOREIGN KEY(idrecipe) REFERENCES recipe(id);
+
+
+
+ALTER TABLE chude_theloai ADD FOREIGN KEY(idchude) REFERENCES chude(id);
+ALTER TABLE chude_theloai ADD FOREIGN KEY(idtheloai) REFERENCES theloai(id);
+
+
+DELIMITER &&  
+CREATE PROCEDURE SP_ThamgiaEvent(IN iduser INT, IN idevent INT, IN idrecipe INT)
+BEGIN  
+	DECLARE sl INT DEFAULT 0;
+    -- Them vao bang thamgiaevent
+    INSERT INTO thamgiaevent VALUES(iduser, idevent, idrecipe);
+    
+    -- Cap nhat lai so luong tham gia
+    SET sl = (SELECT soluong FROM event WHERE id = idevent);
+    SET sl = sl + 1;
+	UPDATE event SET soluong = sl WHERE id = idevent;
+END &&  
+DELIMITER ;
+
+DELIMITER &&  
+CREATE PROCEDURE SP_Tangluotthich(IN id_recipes INT)
+BEGIN  
+    DECLARE lt INT DEFAULT 0;
+    SET lt = (SELECT luotthich FROM recipe WHERE id = id_recipes);
+    SET lt = lt + 1;
+	UPDATE recipe SET luotthich = lt WHERE id = id_recipes;
+END &&  
+DELIMITER ;
+
+
+
